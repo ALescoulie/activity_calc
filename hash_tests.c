be9bded956ea870c5ac71ad13e3ab15ebdd9b38f
ALescoulie <alescoul@calpoly.edu>
2021-12-26 22:57:26 -0800

    get max coverage

--- hash_tests.c
+++ hash_tests.c
@@ -3,7 +3,6 @@
 //
 
 #include <assert.h>
-#include <AppleTextureEncoder.h>
 #include "hash.h"
 
 void test_table_create() {
@@ -55,11 +54,45 @@ void test_table_remove() {
     table_destroy(table);
 }
 
+void test_table_remove1() {
+    HashTable *table = table_create(10);
+    table_remove(table, "H");
+    table_add(table, 1.011, "H");
+    table_remove(table, "He");
+    table_destroy(table);
+}
+
+void test_collision() {
+    int code1[] = {1, 0};
+    int code2[] = {11, 0};
+    int code3[] = {21, 0};
+    char* str1 = (char*)code1;
+    char* str2 = (char*)code2;
+    char* str3 = (char*)code3;
+    HashTable *table = table_create(10);
+    table_add(table, 1.011, str1);
+    table_add(table, 4.00, str2);
+    table_add(table, 6.94, str3);
+    assert(table_get(table, str1) == 1.011);
+    assert(table_get(table, str2) == 4.00);
+    assert(table_get(table, str3) == 6.94);
+    table_remove(table, str3);
+    table_remove(table, str3);
+    table_remove(table, str2);
+    assert(table->size == 1);
+    assert(table_get(table, str1) == 1.011);
+    assert(table_get(table, str2) == 1);
+    table_destroy(table);
+}
+
+
 int main() {
     test_table_create();
     test_add_get0();
     test_add_get1();
     test_table_expand();
     test_table_remove();
+    test_collision();
+    test_table_remove1();
     return 0;
 }
